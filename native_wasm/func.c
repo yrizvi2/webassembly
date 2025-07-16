e0bbde46828af2dfd9ac388f76139f80cf0fe362
yrizvi3 <yrizvi3@gatech.edu>
2025-07-16 12:53:51 -0400

    func.c

--- native_wasm/func.c
+++ native_wasm/func.c
@@ -3,23 +3,23 @@
 
 #define WASM_EXPORT(name) __attribute__((used)) __attribute__((export_name(name)))
 
-// Declare the external funcref table
-extern void *__funcref_table __attribute__((import_module("env")))
-                                __attribute__((import_name("table")));
-
 typedef void (*TFunc)(uint32_t);
+
+// Declare imported funcref table from the host
+extern void *__funcref_table __attribute__((import_module("env")))
+                             __attribute__((import_name("table")));
 
 void print_num(uint32_t x) {
     printf("you gave me = %d\n", x);
 }
 
-// Exported function so host or other Wasm modules can call
+// Exported function to be stored in table
 WASM_EXPORT("print_num")
 void print_num_export(uint32_t x) {
     print_num(x);
 }
 
-// Set up the table with our function pointer at index 0
+// Store function into table at index 0
 WASM_EXPORT("setup_table")
 void setup_table() {
     if (__builtin_wasm_table_size(__funcref_table) == 0) {
@@ -29,7 +29,7 @@ void setup_table() {
     }
 }
 
-// Export a way to tell the host what table index to use
+// Return index of function in table
 WASM_EXPORT("addr")
 uint32_t addr() {
     return 0;
